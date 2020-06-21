a368a1720f068233f7064d1b2f00844b54a396a9
jake <rencelaw70@gmail.com>
2020-06-21 13:19:38 +0800

    Initial Commit

---
+++ public/script.js
@@ -0,0 +1,96 @@
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+// Sticky Navbar
+const sticky = document.querySelector(".main-nav");
+const home = document.querySelector(".home");
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
+window.addEventListener("scroll", function () {
+  if (scrollY > 40) {
+    sticky.classList.add("sticky-navbar");
+  } else {
+    sticky.classList.remove("sticky-navbar");
+  }
+});
+window.addEventListener("scroll", function () {
+  if (scrollY < 600) {
+    home.classList.add("current");
+    contact.classList.remove("current");
+    about.classList.remove("current");
+  }
+  if (scrollY > 600 && scrollY < 1946) {
+    about.classList.add("current");
+    home.classList.remove("current");
+    contact.classList.remove("current");
+  }
+  if (scrollY > 2499) {
+    contact.classList.add("current");
+    about.classList.remove("current");
+  }
+});
+
+// Backend;
